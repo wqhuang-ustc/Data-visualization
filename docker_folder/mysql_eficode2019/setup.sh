9d3c21a26750a0aad77bd563e238b1d2e64e3d41
root <root@ip-172-31-38-54.eu-north-1.compute.internal>
2019-02-21 18:14:09 +0000

    work with mysql container

--- docker_folder/mysql_eficode2019/setup.sh
+++ docker_folder/mysql_eficode2019/setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 set -e
 
-# check mysql status for debuging purpose
-echo `service mysql status`
-
 echo '1. start mysql....'
 #start mysql
 service mysql start
+#systemctl start mysql
 sleep 3
-echo `service mysql status`
 
 echo '2. execute grants.sql file....'
 
@@ -16,7 +13,5 @@ mysql < /mysql/grants.sql
 echo 'add grants'
 
 sleep 3
-echo `service mysql status`
-
 
 tail -f /dev/null
