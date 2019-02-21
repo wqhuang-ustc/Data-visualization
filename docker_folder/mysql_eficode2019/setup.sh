bf17962b1d7b903d5f4c6fc9845876c422686188
wqhuang-ustc <wqhuang.ustc@gmail.com>
2019-02-21 16:32:06 +0100

    customize mysql container to fix accessing problem

---
+++ docker_folder/mysql_eficode2019/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# check mysql status for debuging purpose
+echo `service mysql status`
+
+echo '1. start mysql....'
+#start mysql
+service mysql start
+sleep 3
+echo `service mysql status`
+
+echo '2. execute grant.sql file....'
+
+mysql < /mysql/grants.sql
+echo 'add grants'
+
+sleep 3
+echo `service mysql status`
+
+
+tail -f /dev/null
