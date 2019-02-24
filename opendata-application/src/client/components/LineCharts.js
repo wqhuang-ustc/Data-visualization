b60c48777c236bc705a7014215c5933480c802ad
wqhuang-ustc <wqhuang.ustc@gmail.com>
2019-02-24 11:17:31 +0100

    extend to 4 sensors' data

--- opendata-application/src/client/components/LineCharts.js
+++ opendata-application/src/client/components/LineCharts.js
@@ -7,32 +7,68 @@ ReactChartkick.addAdapter(Chart);
 
 //format opendata into react-chartkick date format
 const allSensor1 = [];
+const allSensor2 = [];
+const allSensor3 = [];
+const allSensor4 = [];
 
 for (let i = 0; i < OpenData.length; i += 1){
     allSensor1.push([OpenData[i].date, OpenData[i].sensor1]);
+}
+
+for (let i = 0; i < OpenData.length; i += 1){
+    allSensor2.push([OpenData[i].date, OpenData[i].sensor2]);
+}
+
+for (let i = 0; i < OpenData.length; i += 1){
+    allSensor3.push([OpenData[i].date, OpenData[i].sensor3]);
+}
+
+for (let i = 0; i < OpenData.length; i += 1){
+    allSensor4.push([OpenData[i].date, OpenData[i].sensor4]);
 }
 
 class LineCharts extends Component {
     constructor(props){
         super(props);
         this.state = {
-            height: '172px',
-            DataSensor1: allSensor1
+            height: '145px',
+            DataSensor1: allSensor1,
+            DataSensor2: allSensor2,
+            DataSensor3: allSensor3,
+            DataSensor4: allSensor4
         };
     }
 
     render(){
-        const {height,DataSensor1} = this.state;
+        const {height,DataSensor1, DataSensor2, DataSensor3, DataSensor4} = this.state;
 
         return (
             <div>
-                <h1>sensor1 data</h1>
+                <h1>Data visualization of 4 sensors</h1>
                 <LineChart
                   data={DataSensor1}
                   label="value"
                   ytitle="Sensor 1"
                   height={height}
                 />
+                <LineChart
+                  data={DataSensor2}
+                  label="value"
+                  ytitle="Sensor 2"
+                  height={height}
+                />
+                <LineChart
+                  data={DataSensor3}
+                  label="value"
+                  ytitle="Sensor 3"
+                  height={height}
+                />
+                <LineChart
+                  data={DataSensor4}
+                  label="value"
+                  ytitle="Sensor 4"
+                  height={height}
+                />   
             </div>
 
         )
